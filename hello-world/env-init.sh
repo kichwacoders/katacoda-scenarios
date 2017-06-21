2490e9156a7d6de6da85850fbd7098659bcabecb
ArtKelly <artandkelly12@gmail.com>
2017-06-21 10:00:11 +0100

    Update env-init.sh

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -3,5 +3,6 @@ Curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/re
 Curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.apache.commons.math3_3.5.0.v20160301-1110.jar > ArtKelly.zip
 Curl http://download.eclipse.org/tools/orbit/downloads/drops/R20170303204511/repository/plugins/org.apache.commons.lang_2.6.0.v201404270220.jar > ArtKelly.zip
 Unzip ArtKelly.zip
+rm ArtKelly.zip
 jshell
 
