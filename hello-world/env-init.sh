e5e26edbbc2b70cf1d6cea31a55531b8470d2a87
ArtKelly <artandkelly12@gmail.com>
2017-06-21 12:09:35 +0100

    Update env-init.sh

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,8 +1,8 @@
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.api_1.7.10.v20170428-1633.jar > /home/scrapbook/tutorial/org.slf4j.api_1.7.10.v20170428-1633.jar
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.binding.nop_1.7.10.v20160301-1109.jar > /home/scrapbook/tutorial/org.slf4j.binding.nop_1.7.10.v20160301-1109.jar
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.apache.commons.math3_3.5.0.v20160301-1110.jar > /home/scrapbook/tutorial/org.apache.commons.math3_3.5.0.v20160301-1110.jar
-curl http://download.eclipse.org/tools/orbit/downloads/drops/R20170303204511/repository/plugins/org.apache.commons.lang_2.6.0.v201404270220.jar > /home/scrapbook/tutorial/org.apache.commons.lang_2.6.0.v201404270220.jar
-curl http://download.eclipse.org/january/releases/2.0.2/repository/plugins/org.eclipse.january_2.0.2.v201706051401.jar > /home/scrapbook/tutorial/org.eclipse.january_2.0.2.v201706051401.jar
+wget http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.api_1.7.10.v20170428-1633.jar 
+wget http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.binding.nop_1.7.10.v20160301-1109.jar
+wget http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.apache.commons.math3_3.5.0.v20160301-1110.jar
+wget http://download.eclipse.org/tools/orbit/downloads/drops/R20170303204511/repository/plugins/org.apache.commons.lang_2.6.0.v201404270220.jar
+wger http://download.eclipse.org/january/releases/2.0.2/repository/plugins/org.eclipse.january_2.0.2.v201706051401.jar
 jshell  --class-path org.eclipse.january_2.0.2.v201706051401.jar:org.apache.commons.lang_2.6.0.v201404270220.jar:org.apache.commons.math3_3.5.0.v20160301-1110.jar:org.slf4j.api_1.7.10.v20170428-1633.jar:org.slf4j.binding.nop_1.7.10.v20160301-1109.jar
 
 
