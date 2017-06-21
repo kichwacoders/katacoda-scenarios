6d26c5cf139b505f3763a71d11e6331777ef20c7
ArtKelly <artandkelly12@gmail.com>
2017-06-21 10:37:14 +0100

    Update env-init.sh

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,8 +1,6 @@
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.api_1.7.10.v20170428-1633.jar > ArtKelly.zip
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.binding.nop_1.7.10.v20160301-1109.jar > ArtKelly.zip
-curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.apache.commons.math3_3.5.0.v20160301-1110.jar > ArtKelly.zip
-curl http://download.eclipse.org/tools/orbit/downloads/drops/R20170303204511/repository/plugins/org.apache.commons.lang_2.6.0.v201404270220.jar > ArtKelly.zip
-unzip ArtKelly.zip
-rm ArtKelly.zip
+curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.api_1.7.10.v20170428-1633.jar > First.jar
+curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.slf4j.binding.nop_1.7.10.v20160301-1109.jar > Second.jar
+curl http://download.eclipse.org/tools/orbit/downloads/drops2/R20170516192513/repository/plugins/org.apache.commons.math3_3.5.0.v20160301-1110.jar > Third.jar
+curl http://download.eclipse.org/tools/orbit/downloads/drops/R20170303204511/repository/plugins/org.apache.commons.lang_2.6.0.v201404270220.jar > Fourth.jar
 jshell
 
